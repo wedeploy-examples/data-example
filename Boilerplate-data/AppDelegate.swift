08c67410202ec22285608471e4de7d59a5e00842
Víctor Galán <gagranta@gmail.com>
2017-04-10 12:59:18 +0200

    Initial project

---
+++ Boilerplate-data/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Boilerplate-data
+//
+//  Created by Víctor Galán on 10/4/17.
+//  Copyright © 2017 liferay. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'Boilerplate-data' do
+  use_frameworks!
+  pod 'WeDeploy', :git => 'https://github.com/victorg1991/api-swift'
+end
